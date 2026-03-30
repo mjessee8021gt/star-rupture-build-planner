5d807ae6539d1596d96b23ff91c99c63b4673691
Matt Jessee <mattgjessee@gmail.com>
2026-03-29 18:17:46 -0700

    Fixing the patchregistry from corruption

--- Scripts/PatchRegistry.gd
+++ Scripts/PatchRegistry.gd
@@ -1,6 +1,6 @@
 extends Resource
 #BUildingRegistry holds the enumaeration for all buildings in the tool. This ensures enumeration IDs remain constant through the entire program.
-class_name PatchRegistry
+class_name PatchRegistry##comment to reset the coorrupted commit
 @export var patch_notes: Array[PatchNote] = []
 
 func get_patch_notes() ->Array[PatchNote]:
